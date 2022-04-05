e942fc388e892b89e48ee689163f70646f9f019e
Saiful AKbar <saifulakbar.dev@gmail.com>
2022-04-05 09:51:08 +0700

    Fix metadata theme

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -3,48 +3,21 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-    />
-    <meta
-        name="description"
-        content="Sistem informasi manajemen anggaran & arsip"
-    />
-    <meta
-        name="author"
-        content="Saiful Akbar"
-    />
-    <meta
-        name="base-url"
-        content="{{ url('/') }}"
-    />
-    <meta
-        name="csrf-token"
-        content="{{ csrf_token() }}"
-    />
-    <meta
-        name="theme"
-        content="{{ auth()->user()->pengaturan->tema }}"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Sistem informasi manajemen anggaran & arsip" />
+    <meta name="author" content="Saiful Akbar" />
+    <meta name="base-url" content="{{ url('/') }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="theme" content="{{ auth()->user() ? auth()->user()->pengaturan->tema : 'light' }}" />
 
     {{-- Title --}}
-    <title>
-        @yield('title') : {{ config('app.name') }}
-    </title>
+    <title>@yield('title') : {{ config('app.name') }}</title>
 
     {{-- App favicon --}}
-    <link
-        href="{{ asset('assets/images/logo/favicon.ico') }}"
-        rel="shortcut icon"
-    />
+    <link href="{{ asset('assets/images/logo/favicon.ico') }}" rel="shortcut icon" />
 
     {{-- icons --}}
-    <link
-        href="{{ asset('assets/css/icons.min.css') }}"
-        rel="stylesheet"
-        type="text/css"
-    />
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 
     {{-- costom vendor css --}}
     @stack('css')
@@ -52,49 +25,25 @@
     {{-- App css auth --}}
     @auth
         @if (auth()->user()->pengaturan->tema == 'dark')
-            <link
-                href="{{ asset('assets/css/app-dark.min.css') }}"
-                rel="stylesheet"
-                type="text/css"
-                id="dark-style"
-            />
+            <link href="{{ asset('assets/css/app-dark.min.css') }}" rel="stylesheet" type="text/css" id="dark-style" />
         @else
-            <link
-                href="{{ asset('assets/css/app.min.css') }}"
-                rel="stylesheet"
-                type="text/css"
-                id="light-style"
-            />
+            <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="light-style" />
         @endif
     @endauth
 
     {{-- App css guest --}}
     @guest
-        <link
-            href="{{ asset('assets/css/app.min.css') }}"
-            rel="stylesheet"
-            type="text/css"
-            id="light-style"
-        />
+        <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="light-style" />
     @endguest
 
     {{-- Main css --}}
-    <link
-        href="{{ asset('assets/css/main.min.css') }}"
-        rel="stylesheet"
-        type="text/css"
-    />
+    <link href="{{ asset('assets/css/main.min.css') }}" rel="stylesheet" type="text/css" />
 
     {{-- Page css --}}
     @yield('css')
 </head>
 
-<body
-    class="loading @guest authentication-bg pb-0 @endguest"
-    @auth
-        data-leftbar-theme="{{ auth()->user()->pengaturan->sidebar }}"
-    @endauth
->
+<body class="loading @guest authentication-bg pb-0 @endguest" @auth data-leftbar-theme="{{ auth()->user()->pengaturan->sidebar }}" @endauth>
 
     {{-- Pre-loader --}}
     <div id="preloader">
