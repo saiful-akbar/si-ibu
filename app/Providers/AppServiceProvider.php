557e0ab081d5a91de54e2eb8d522ae6fb4a92d96
Saiful Akbar <saifulakbar.dev@gmail.com>
2023-06-05 20:12:33 +0700

    Disable sanctum default migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Sanctum\Sanctum;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Disable sanctum default migration
+        Sanctum::ignoreMigrations();
     }
 
     /**
